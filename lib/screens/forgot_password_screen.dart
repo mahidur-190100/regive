adb25414271d1cc9ab66d2fdd97973514c186e63
Mahidur Rahman <mahidur952697@gmail.com>
2026-08-10 23:10:32 +0600

    forget password create

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -7,7 +7,7 @@ class ForgotPasswordScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Forgot Password')),
-      body: const Center(child: Text('Forgot Password screen — coming next')),
+      body: const Center(child: Text('Forget password coming soon Please wait')),
     );
   }
 }
